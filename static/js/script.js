7b6fc63d8b25e24d3e29cdac907b15cb31afa4f6
Teja Vunnam <vunnamteja@gmail.com>
2021-06-11 15:12:53 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,6 @@ $(function(){
 			data: $('form').serialize(),
 			type: 'POST',
 			success: function(response){
-				console.log(response);
 			},
 			error: function(error){
 				console.log(error);
